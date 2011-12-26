072e508d9a4a17c3aedce9f337119dcaefec873c
Lutz Hankewitz <lutz@hankewitz.de>
2011-12-26 01:15:51 +0100

    replaced check of equal to '\n' with platform dependent line separator.

--- chap10/nano-http-server/src/test/scala/ServerSpecs.scala
+++ chap10/nano-http-server/src/test/scala/ServerSpecs.scala
@@ -5,11 +5,13 @@ class ServerSpecs extends Specification {
   "Light Server" should {
     "return the contents of available resource" in {
       val response = get("GET /test.txt HTTP/1.0\n".toIterator)(name => Server.IOResource(name))
+	  response.foreach{println}
+	  println("'" + response(4) + "'")
       response(0) must beEqualTo("HTTP/1.1 200 OK") 
       response(1) must beEqualTo("Date " + new java.util.Date) 
       response(2) must beEqualTo("Content-Type: text/html") 
       response(3) must beEqualTo("Content-Length: " + Source.fromFile("test.txt").count(x => true)) 
-      response(4) must beEqualTo("\n") 
+	  response(4) must beEqualTo(System.getProperty("line.separator"))
       response(5) must beEqualTo("<html>")
       response(6) must beEqualTo("<body>")
       response(7) must beEqualTo("This is a sample response")
