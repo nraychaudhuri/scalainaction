d65e5ea1b95de2d39138d4cbedd4766c3867a5da
Lutz Hankewitz <lutz@hankewitz.de>
2012-01-05 23:43:51 +0100

    enabled JavaScalaActors for sbt 0.11.2 and provided the neccessary 2.9.1 scala libs for a pure java eclipse project

---
+++ chap08/JavaScalaActors/build.sbt
@@ -0,0 +1,11 @@
+name := "JavaScalaActors"
+
+version := "1.0"
+
+organization := "Scala in Action"
+
+javacOptions ++= Seq("-source", "1.6")
+
+
+
+
--- chap08/scala_actors_wrapper/src/main/scala/ScalaActorWrapper.scala
+++ chap08/scala_actors_wrapper/src/main/scala/ScalaActorWrapper.scala
@@ -7,5 +7,19 @@ package com.javaapi.forscala.actors {
   trait Effect[A] {
     def e(thisActor: Actor, msg: A): Unit
   }
-
+  
+  object ScalaActorWrapper {
+	def actor[A](effect: Effect[A]) = {
+		Actor.actor {
+			loop {
+				react {
+					case Exit(from, reason) =>
+						println("exit message received from " + from)
+						Actor.exit
+					case msg: A => effect.e(self, msg)
+				}
+			}
+		}
+	}
+  }
 }
