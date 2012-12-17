08101429410362b20f4167db164a8dcb8dcdbed0
Nilanjan Raychaudhuri <nraychaudhuri@gmail.com>
2012-12-16 19:30:48 -0500

    Cleanup for chapter 3

--- chap03/scala-mongo-driver-sbt/build.sbt
+++ chap03/scala-mongo-driver-sbt/build.sbt
@@ -4,7 +4,7 @@ scalaVersion := "2.9.1"
 
 organization := "Scala in Action"
 
-libraryDependencies += "org.mongodb" % "mongo-java-driver" % "1.3"
+libraryDependencies += "org.mongodb" % "mongo-java-driver" % "2.10.1"
 
  
  // append options passed to the Scala compiler
--- chap05/fp-examples/src/main/scala/ComninatorsExample.scala
+++ chap05/fp-examples/src/main/scala/ComninatorsExample.scala
@@ -19,10 +19,9 @@ case class Mailer(mailAddress: String) {
 object Main {
   import Combinators._
   def main(args: Array[String]): Unit = {
-    val mailer = Mailer("some address")
     Person("Nilanjan", "Raychaudhuri").tap(p => {
      println("First name " + p.firstName) 
-     mailer.mail("new person joined " + p)
+     Mailer("some address").mail("new person joined " + p)
     }).lastName
   }
 }
