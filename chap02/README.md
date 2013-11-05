e4585d56c1b49169bf4ca8d7870fb86f675bab94
Nilanjan Raychaudhuri <nraychaudhuri@gmail.com>
2013-11-04 21:56:13 -0500

    added instructions for running rest client

---
+++ chap02/README.md
@@ -0,0 +1,3 @@
+## Steps to build and run REST client
+
+To run the REST client use the sbt shell script provided under restclient project folder.
--- chap02/rest.bat
+++
@@ -1,3 +0,0 @@
-scala -cp ./httpclient-4.1.2.jar;./httpcore-4.1.2.jar;./commons-logging-1.1.1.jar RestClient.scala %1 %2 %3 %4 %5 %6
-          
-		 
